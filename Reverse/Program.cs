038798891d4914c99180c6fe65224f9ed456dd97
anita.thomas <thomassara1071@gmail.com>
2023-02-14 16:20:11 +0530

    first commit

---
+++ Reverse/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Reverse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string Str, rev;
+            Str = "ANITA MARY THOMAS";
+            rev = "";
+            Console.WriteLine("Value of given String is: {0}", Str);
+            int a;
+            a = Str.Length - 1;
+            while (a >= 0)
+            {
+                rev = rev + Str[a];
+                a--;
+            }
+            Console.WriteLine("Reversed string is: {0}", rev);
+            Console.ReadLine();
+        }
+    }
+}
